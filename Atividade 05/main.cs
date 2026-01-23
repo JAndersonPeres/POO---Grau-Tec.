55ef4e570d3307476589bbf6247bb1e6be9c2a55
Aluno Grau <alunograu@tecnico.com>
2026-01-23 08:53:47 -0300

    Aula 2026_04_21 - Final Commit

---
+++ Atividade 05/main.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+
+namespace ATV5
+{
+    public class AcessoNegadoException : Exception
+    {
+        public AcessoNegadoException(string msg) : base(msg){}
+    }
+
+    public class SistemaAuth
+    {
+        private Dictionary<string,string> bancoDeDados = new Dictionary<string, string>();
+
+        public SistemaAuth()
+        {
+            bancoDeDados.Add("admin","admin");
+            bancoDeDados.Add("Aluno","gAluno123");
+            bancoDeDados.Add("Professor","gProf456");
+        }
+
+        public void FazerLogin(string usuario, string senha)
+        {
+            if (!bancoDeDados.ContainsKey(usuario))
+            {
+                throw new AcessoNegadoException("Usuário não Cadastrado.");
+            }
+
+            if(bancoDeDados[usuario] != senha)
+            {
+                throw new AcessoNegadoException("Senha Inválida|");
+            }
+
+            Console.WriteLine($"[SUCESSO] Bem-vindo, {usuario}!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SistemaAuth sistema = new SistemaAuth();
+
+            Console.WriteLine("---TENTATIVA 1(Correta)---");
+            try
+            {
+                sistema.FazerLogin("admin","admin");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
+
+            Console.WriteLine("---TENTATIVA 2(Senha Errada)---");
+            try
+            {
+                sistema.FazerLogin("Aluno","Aluno@GrauT");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[ALERTA DE SEGURANÇA] {ex.Message}");
+            }
+
+            Console.WriteLine("---TENTATIVA 3(Usuário Inexistente)---");
+            try
+            {
+                sistema.FazerLogin("Edvaldo","Edv@250");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[ERRO] {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("\nProcesso de validação finalizado");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
